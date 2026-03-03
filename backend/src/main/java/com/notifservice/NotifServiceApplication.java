3970f08059fca706201ea349299c0f38b59df977
Vamshi Krishna Samala <vamshikrishb15@gmail.com>
2026-03-03 20:15:00 +0530

    project setup

---
+++ backend/src/main/java/com/notifservice/NotifServiceApplication.java
@@ -0,0 +1,12 @@
+package com.notifservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifServiceApplication.class, args);
+    }
+}
